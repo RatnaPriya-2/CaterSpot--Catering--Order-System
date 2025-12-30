08c05d485ce7fca4c4d7e91bd4c1aadbf76c6cc2
RatnaPriya-2 <ratnapriya.t@gmail.com>
2025-12-30 17:30:13 +0530

    done

--- admin/adminLayout/adminLayout.js
+++ admin/adminLayout/adminLayout.js
@@ -37,11 +37,11 @@ navLinks.forEach((link) => {
     cssLink.rel = "stylesheet";
     cssLink.href = `/admin/${target}/${target}.css`;
     document.head.appendChild(cssLink);
+    targetSection.classList.remove("hidden");
     // load html
     const response = await fetch(`/admin/${target}/${target}.html`);
     const html = await response.text();
     targetSection.innerHTML = html;
-    targetSection.classList.remove("hidden");
 
     // dynamically import JS module
     const module = await import(`/admin/${target}/${target}.js`);
